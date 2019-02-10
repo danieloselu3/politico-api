46f799625b10eedf108533354fe5aefe4509a255
danieloselu3 <doselu3@gmail.com>
2019-02-10 11:05:33 +0300

    [modified #163721901] renamed app to src

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-from app import create_app
+from src import create_app
 
 environment = os.getenv('FLASK_CONFIG', 'development')
 app = create_app(environment)
--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config[config_name])
 
-    from app.main import bp as main_bp
+    from src.main import bp as main_bp
     app.register_blueprint(main_bp)
 
     return app
